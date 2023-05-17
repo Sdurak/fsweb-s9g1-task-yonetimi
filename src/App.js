731b8ba87b4ad17155039a49d069b0deb4a1872a
Sdurak <sdurak91@hotmail.com>
2023-05-17 19:37:51 +0300

    S9G1 Proje

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
 import { useState } from "react";
-import "./app.css";
+import { ToastContainer, toast } from "react-toastify";
+import { initialTasks, initialTeam } from "./data";
 import Task from "./Task";
 import TaskForm from "./TaskForm";
 import TaskHookForm from "./TaskHookForm";
 import PeopleForm from "./PeopleForm";
-import { initialTasks, initialTeam } from "./data";
-
+import "./app.css";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
+  const notify = (text) => toast("heyy!" + text);
   const [tasks, setTasks] = useState(initialTasks);
   const [team, setTeam] = useState(initialTeam);
 
   function handleTaskSubmit(yeniTask) {
-    setTasks([yeniTask, ...tasks])
+    setTasks([yeniTask, ...tasks]);
+    notify(yeniTask.title + "yapılmak için bekliyor");
   }
 
   function handlePeopleSubmit(yeniKisi) {
-    setTeam([...team, yeniKisi])
+    notify(yeniKisi + "iş kitlemek için hazır");
+    setTeam([...team, yeniKisi]);
   }
 
   function handleComplete(id) {
-    console.log("tamamlama fonksiyonunu buraya yazın")
+    console.log("tamamlama fonksiyonunu buraya yazın", id);
+    const tasksClone = [...tasks];
+    const updatedTask = tasksClone.find((t) => t.id === id);
+    updatedTask.status = "yapıldı";
+    console.log("updatedTask", updatedTask);
+    notify(updatedTask.title + "tamamlandı!");
+    setTasks(tasksClone);
   }
 
   return (
@@ -59,9 +69,26 @@ function App() {
           </div>
         </div>
       </div>
-
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
     </div>
   );
 }
 
 export default App;
+
+// console.log("tamamlama fonksiyonunu buraya yazın");
+// const taskKopya = [...tasks];
+// const tiklananTask = taskKopya.filter((olay) => olay.id === id)[0];
+// tiklananTask.status = "yapıldı";
+// setTasks(taskKopya);
